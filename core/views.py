15827792ad4f8e4a38759ef0d672683874e3bce5
yaswanth <chukkayaswanth12@gmail.com>
2025-04-23 00:37:07 +0530

    Day 2

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_view(request):
+    return HttpResponse("<h1>Welcome to the home page!</h1>")
+
+def about_view(request):
+    return HttpResponse("<h1>About Us</h1><p>This is the about page.</p>")
